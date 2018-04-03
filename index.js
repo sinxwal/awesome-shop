cc4a189a7ffa96fba74fb784f1fbc9217a5226fe
Sergey Tskhovrebov <sinxwal@gmail.com>
2018-04-03 22:55:35 +0300

    added hapi

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+'use strict'
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+const Hapi = require('hapi'),
+    routes = require('./server/routes')
+
+const port = process.env.PORT || 7000
+
+const server = Hapi.server({
+  host: 'localhost',
+  port,
+})
+
+// Adding client route.
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    return h.file('client/index.html')
+  },
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// Adding server routes.
+server.route(routes(server))
+
+const init = async () => {
+  await server.register([
+    require('inert'),
+    {
+      plugin: require('hapi-pino'),
+      options: {
+        prettyPrint: false,
+        logEvents: ['response'],
+      },
+    },
+  ])
+
+  await server.start()
+  console.log(`Server running at: ${server.info.uri}`)
+}
+
+init()
---
+++ server/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const routes = (server) => [{
+    method: 'GET',
+    path: '/api',
+    handler: (request, h) => {
+        return "api";
+    }
+}];
+
+module.exports = routes;
